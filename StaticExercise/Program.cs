07d2b172aacef5d4448d9b017e6e5a574f99742b
Brett Bellomy <brettbellomymusic@gmail.com>
2024-05-14 11:57:29 -0500

    first commit: completed assignment

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please enter a temperature (in fahreheit) that you would like converted to celcius");
+            var fahrenheit = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"{fahrenheit}F is equal to {TempConverter.FahrenheitToCelcius(fahrenheit)}C");
+
+            Console.WriteLine("Please enter a temperature (in celcius) that you would like converted to fahrenheit.");
+            var celcius = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"{celcius}C is equal to {TempConverter.CelciusToFahrenheit(celcius)}F");  
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelcius(double fahrenheit)
+        {
+            var result = (fahrenheit - 32) / 1.8;
+            return result;
+        }
+
+        public static double CelciusToFahrenheit(double celcius)
+        {
+            var result = (celcius * 1.8) + 32;
+            return result;
+        }
+    }
+}
